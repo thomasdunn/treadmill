9e985353152d7f46434f9cc945378a9801bfde4a
Tom Dunn <tdunn@artofcontext.com>
2021-07-25 00:40:29 -0400

    Implements showing elapsed time.

--- src/app/manual/manual.page.ts
+++ src/app/manual/manual.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { State, StateService } from '../state.service';
+import { Subject, timer, interval } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-manual',
@@ -10,7 +12,10 @@ export class ManualPage {
 
   public stateService: StateService;
 
-  speedSetting = 0;
+  stopSubject = new Subject();
+
+  speedSetting = 31;
+  seconds = 0;
 
   constructor(stateService: StateService) {
     this.stateService = stateService;
@@ -22,13 +27,31 @@ export class ManualPage {
 
   stop() {
     this.stateService.setState(State.stopped);
+    this.stopSubject.next();
   }
 
   start() {
+    if (this.stateService.getState() === State.stopped) {
+      this.seconds = 0;
+    }
+
     this.stateService.setState(State.running);
+
+    interval(1000)
+      .pipe(takeUntil(this.stopSubject))
+      .subscribe(seconds => {
+        this.seconds++;
+      });
   }
 
   pause() {
     this.stateService.setState(State.paused);
+    this.stopSubject.next();
+  }
+
+  getElapsed() {
+    const min = Math.floor(this.seconds / 60) + '';
+    const sec = (this.seconds % 60) + '';
+    return `${min.padStart(2, '0')}:${sec.padStart(2, '0')}`;
   }
 }
