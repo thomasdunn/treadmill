72cdbf5af585ff7e9284715cb5f804a452d14aac
Tom Dunn <thomas.dunn@gmail.com>
2021-07-30 03:22:53 +0100

    Communicate between client and server with socket io.  Implement sending speed setting.

--- ionic/src/app/app.module.ts
+++ ionic/src/app/app.module.ts
@@ -4,13 +4,20 @@ import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+
+import { environment } from '../environments/environment';
+
+// const socketConfig: SocketIoConfig = { url: environment.serviceUrl, options: {} };
+const socketConfig: SocketIoConfig = { url: null };
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, SocketIoModule.forRoot(socketConfig)],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- service/index.js
+++ service/index.js
@@ -2,9 +2,23 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.use(express.static('../ionic/www'));
 
 server.listen(80, () => {
   console.log('listening on *:80');
+});
+
+io.on('connection', (socket) => {
+  console.log('web client connected');
+
+  socket.on('set-speed', speed => {
+    const speedValue = parseInt(speed);
+
+    if (! isNaN(speedValue) && speedValue >= 0 && speedValue <= 127) {
+      console.log(`Speed: ${speed}`);
+    }
+  });
 });
